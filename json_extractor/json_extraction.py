47b0b8eb672b3435187076b44dd30c86b997441a
jason2714 <jason27146913@gmail.com>
2024-12-07 18:47:45 +0800

    feat: add functionality to extract formatted data

---
+++ json_extractor/json_extraction.py
@@ -0,0 +1,43 @@
+import json
+from pathlib import Path
+
+from openai_client import OpenAIClient
+from prompt import example_prompt, extraction_prompt
+from utils import read_json_template, read_raw_data, save_json_result
+
+
+def get_template():
+    template_dir = Path('templates')
+    template_filename = 'template_i.json'
+    template_path = template_dir / template_filename
+    template = read_json_template(template_path)
+    return template
+
+
+def get_raw_data():
+    raw_data_path = Path('raw_data.txt')
+    raw_data = read_raw_data(raw_data_path)
+    return raw_data
+
+
+def get_example():
+    example = example_prompt.DOCLING_EXAMPLE
+    return example
+
+
+def main():
+    llm_client = OpenAIClient()
+    template = get_template()
+    raw_data = get_raw_data()
+    example = get_example()
+    system_prompt = extraction_prompt.SYSTEM_PROMPT
+    user_prompt = extraction_prompt.USER_PROMPT.format(example=example,
+                                                       raw_data=raw_data,
+                                                       template=template)
+    response = llm_client.request(system_prompt, user_prompt)
+    print(response)
+    # save_json_result(Path('output.json'), json.loads(response))
+
+
+if __name__ == '__main__':
+    main()
---
+++ json_extractor/openai_client.py
@@ -0,0 +1,51 @@
+import os
+import traceback
+
+import openai
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+class OpenAIClient:
+    def __init__(self, model="gpt-4o"):
+        """
+        OpenAIClient for interacting with the OpenAI API.
+
+        Args:
+            model (str): The model to use (default: "gpt-4o").
+        """
+        self.model = model
+        load_dotenv()
+        api_key = os.getenv('OPENAI_API_KEY')
+        if not api_key:
+            raise ValueError("OpenAI API key not found. Please set it in the .env file.")
+        self.client = OpenAI(api_key=api_key)
+
+    def request(self, system_prompt, user_prompt):
+        """
+        Generate a response from the OpenAI API.
+
+        Args:
+            system_prompt (str): The system prompt for context.
+            user_prompt (str): The user prompt for input.
+
+        Returns:
+            str: The API-generated response.
+        """
+        try:
+            completion = self.client.chat.completions.create(
+                model=self.model,
+                messages=[
+                    {"role": "system", "content": system_prompt},
+                    {
+                        "role": "user",
+                        "content": user_prompt
+                    }
+                ],
+                temperature=0.0
+            )
+            return completion.choices[0].message.content
+        except openai.OpenAIError:
+            return f"OpenAI API error: {traceback.format_exc()}"
+        except Exception:
+            return f"An unexpected error occurred: {traceback.format_exc()}"
